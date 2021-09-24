69044796334eef8c091fdadc12396528eddb6fee
murytarlah <dknight.jim@gmail.com>
2021-09-24 20:45:44 +0100

    edited js file

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ function searchMeal(e) {
   // Check for empty
   if (term.trim()) {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
-      .then(res => res.json())
+      .then(response => response.json())
       .then(data => {
         console.log(data);
         resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
